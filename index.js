2f4c5ae0a59fbb6d0a01fa373e5bb65cf0667638
jhonnysusaya <jhonny.susaya@gmail.com>
2021-11-06 12:43:23 -0500

    Auth

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express= require('express');
 const cors = require('cors');
 
-const {dbConnection} = require('./database/config');
+const { dbConnection } = require('./database/config');
 
 //Crear el servidor de express
 const app = express();
@@ -11,17 +11,17 @@ const app = express();
 //Configurar CORS
 app.use(cors());
 
+//Lectura y parseo del body
+
+app.use(express.json());
+
 //Base de datos
 
 dbConnection();
 
 //rutas
-app.get('/', (req, res)=>{
-    res.status(400).json({
-        ok:true,
-        msg: 'Hola Mundo'
-    })
-});
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'))
 
 app.listen(process.env.PORT,()=>{
     console.log('Servidor corriendo en puerto ' + process.env.PORT)});
